4c86dae9fe80cd954789e84b5ee333ba2707f1df
muthomi254 <dmunene202020@gmail.com>
2023-10-11 09:53:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ function isPalindrome(word) {
 /* 
   Add your pseudocode here
 */
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("level")); // true
+console.log(isPalindrome("hello")); // false
+
+
+
+function isPalindrome(str) {
+  // Convert the input string to lowercase to ensure case-insensitivity.
+  str = str.toLowerCase();
+  let left = 0;
+  let right = str.length - 1;
+
+  // Loop through the string until the left pointer is less than the right pointer.
+  while (left < right) {
+    // If the characters at the left and right pointers are not the same, return false.
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  // If the loop completes without returning false, the string is a palindrome, so return true.
+  return true;
+}
 
 /*
   Add written explanation of your solution here
